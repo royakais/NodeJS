cf7f9481a4fed1e76e1a917363a9fa37469d2edd
royakais <kaisroya@gmail.com>
2026-09-18 17:42:36 +0300

    Hello World in NodeJS

---
+++ Hello World/hello.js
@@ -0,0 +1,8 @@
+//node.js is a javascript runtime
+//it can excute javascript code outside of a browser (server)
+
+//lets make a simple hello world using javascript language
+
+console.log("hello world from Roya, using NodeJS");
+
+//to run this code,save your file, and open your terminal (ctrl+`) and type: node yourfilename.js
